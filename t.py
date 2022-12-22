dbe3077ca32b2471230b4f7f9735fb9755f42528
Ho-Joong Kim <hojoong_kim@korea.ac.kr>
2022-12-22 18:24:26 +0900

    f

--- t.py
+++ t.py
@@ -11,6 +11,23 @@ transform = create_transform(
     # re_count=config.AUG.RECOUNT,
     # interpolation=config.DATA.INTERPOLATION,
 )
+#     def __call__(self, x):
+#         ops = np.random.choice(
+#             self.ops,
+#             self.N,
+#             # replace=self.choice_weights is None,
+#             # p=self.choice_weights,
+#         )
+#         for op in ops:
+#             x = torch.stack([op(_) for _ in x])
+#         return x
+import cv2
+import numpy as np
+import torch
+from PIL import Image
+
+from datasets.utils import spatial_sampling, tensor_normalize
+from video_transforms import create_random_augment
 
 # print(transform)
 
@@ -49,22 +66,7 @@ transform = create_transform(
 #         print(self.ops)
 #         KA.RandomSolarize(same_on_batch=True),
 
-#     def __call__(self, x):
-#         ops = np.random.choice(
-#             self.ops,
-#             self.N,
-#             # replace=self.choice_weights is None,
-#             # p=self.choice_weights,
-#         )
-#         for op in ops:
-#             x = torch.stack([op(_) for _ in x])
-#         return x
-import cv2
-import numpy as np
-import torch
-from PIL import Image
 
-from video_transforms import create_random_augment
 
 transform =create_random_augment(224, "rand-m7-n4-mstd0.5-inc1", "bilinear")
 
@@ -102,102 +104,8 @@ transform =create_random_augment(224, "rand-m7-n4-mstd0.5-inc1", "bilinear")
 #     # KE.Normalize(mean=mean / 255, std=std / 255),
 # ])
 
-from einops import rearrange, repeat
-
-
-def tensor_normalize(tensor, mean, std):
-    """
-    Normalize a given tensor by subtracting the mean and dividing the std.
-    Args:
-        tensor (tensor): tensor to normalize.
-        mean (tensor or list): mean value to subtract.
-        std (tensor or list): std to divide.
-    """
-    if tensor.dtype == torch.uint8:
-        tensor = tensor.float()
-        tensor = tensor / 255.0
-    if type(mean) == list:
-        mean = torch.tensor(mean)
-    if type(std) == list:
-        std = torch.tensor(std)
-    tensor = tensor - mean
-    tensor = tensor / std
-    return tensor
-
-def spatial_sampling(
-    frames,
-    spatial_idx=-1,
-    min_scale=256,
-    max_scale=320,
-    crop_size=224,
-    random_horizontal_flip=True,
-    inverse_uniform_sampling=False,
-    aspect_ratio=None,
-    scale=None,
-    motion_shift=False,
-):
-    """
-    Perform spatial sampling on the given video frames. If spatial_idx is
-    -1, perform random scale, random crop, and random flip on the given
-    frames. If spatial_idx is 0, 1, or 2, perform spatial uniform sampling
-    with the given spatial_idx.
-    Args:
-        frames (tensor): frames of images sampled from the video. The
-            dimension is `num frames` x `height` x `width` x `channel`.
-        spatial_idx (int): if -1, perform random spatial sampling. If 0, 1,
-            or 2, perform left, center, right crop if width is larger than
-            height, and perform top, center, buttom crop if height is larger
-            than width.
-        min_scale (int): the minimal size of scaling.
-        max_scale (int): the maximal size of scaling.
-        crop_size (int): the size of height and width used to crop the
-            frames.
-        inverse_uniform_sampling (bool): if True, sample uniformly in
-            [1 / max_scale, 1 / min_scale] and take a reciprocal to get the
-            scale. If False, take a uniform sample from [min_scale,
-            max_scale].
-        aspect_ratio (list): Aspect ratio range for resizing.
-        scale (list): Scale range for resizing.
-        motion_shift (bool): Whether to apply motion shift for resizing.
-    Returns:
-        frames (tensor): spatially sampled frames.
-    """
-    assert spatial_idx in [-1, 0, 1, 2]
-    if spatial_idx == -1:
-        if aspect_ratio is None and scale is None:
-            frames, _ = video_transforms.random_short_side_scale_jitter(
-                images=frames,
-                min_size=min_scale,
-                max_size=max_scale,
-                inverse_uniform_sampling=inverse_uniform_sampling,
-            )
-            frames, _ = video_transforms.random_crop(frames, crop_size)
-        else:
-            transform_func = (
-                video_transforms.random_resized_crop_with_shift
-                if motion_shift
-                else video_transforms.random_resized_crop
-            )
-            frames = transform_func(
-                images=frames,
-                target_height=crop_size,
-                target_width=crop_size,
-                scale=scale,
-                ratio=aspect_ratio,
-            )
-        if random_horizontal_flip:
-            frames, _ = video_transforms.horizontal_flip(0.5, frames)
-    else:
-        # The testing is deterministic and no jitter should be performed.
-        # min_scale, max_scale, and crop_size are expect to be the same.
-        assert len({min_scale, max_scale, crop_size}) == 1
-        frames, _ = video_transforms.random_short_side_scale_jitter(
-            frames, min_scale, max_scale
-        )
-        frames, _ = video_transforms.uniform_crop(frames, crop_size, spatial_idx)
-    return frames
-
 import torchvision.transforms as transforms
+from einops import rearrange, repeat
 
 img = Image.open("000000.png")
 buffer = [img for i in range(16)]
@@ -226,25 +134,25 @@ buffer = spatial_sampling(
     spatial_idx=-1,
     min_scale=256,
     max_scale=320,
-    crop_size=self.crop_size,
-    random_horizontal_flip=False if args.data_set == 'SSV2' else True ,
+    crop_size=224,
+    random_horizontal_flip=True,
     inverse_uniform_sampling=False,
     aspect_ratio=asp,
     scale=scl,
     motion_shift=False
 )
 
-if self.rand_erase:
-    erase_transform = RandomErasing(
-        args.reprob,
-        mode=args.remode,
-        max_count=args.recount,
-        num_splits=args.recount,
-        device="cpu",
-    )
-    buffer = buffer.permute(1, 0, 2, 3)
-    buffer = erase_transform(buffer)
-    buffer = buffer.permute(1, 0, 2, 3)
+# if self.rand_erase:
+#     erase_transform = RandomErasing(
+#         args.reprob,
+#         mode=args.remode,
+#         max_count=args.recount,
+#         num_splits=args.recount,
+#         device="cpu",
+#     )
+#     buffer = buffer.permute(1, 0, 2, 3)
+#     buffer = erase_transform(buffer)
+#     buffer = buffer.permute(1, 0, 2, 3)
 
 from torchvision.utils import save_image
 
