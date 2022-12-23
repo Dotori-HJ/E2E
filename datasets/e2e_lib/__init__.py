a7e17ca73c5b1ee312abfdc1082b7bc16be75dfe
Ho-Joong Kim <hojoong_kim@korea.ac.kr>
2022-12-24 03:42:26 +0900

    f

--- datasets/e2e_lib/__init__.py
+++ datasets/e2e_lib/__init__.py
@@ -61,6 +61,7 @@ def make_img_transform(is_training, resize=110, crop=96, mean=127.5, std=127.5, 
         GroupRotate,
     )
 
+    print(resize)
     if isinstance(resize, (list, tuple)):
         resize_trans = GroupResize(resize)
     else:
