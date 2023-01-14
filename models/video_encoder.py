fe0bee08d1df1220346128f00356a93fbb071221
Ho-Joong Kim <hojoong_kim@korea.ac.kr>
2023-01-14 11:08:23 +0900

    f

--- models/video_encoder.py
+++ models/video_encoder.py
@@ -203,7 +203,7 @@ class PreNormResidual(nn.Module):
         return self.fn(self.norm(x)) + x
 
 class MLP(nn.Module):
-    def __init__(self, in_dim, hidden_dim, out_dim, conv=False, pre_norm=False):
+    def __init__(self, in_dim, hidden_dim, out_dim, conv=False, pre_norm=True):
         super().__init__()
         self.pre_norm = pre_norm
         if conv:
