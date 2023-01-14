607cf68ebfe1114d69a0226aeaf240231653d88b
Ho-Joong Kim <hojoong_kim@korea.ac.kr>
2023-01-14 17:13:41 +0900

    f

--- models/video_encoder.py
+++ models/video_encoder.py
@@ -236,8 +236,9 @@ class MLP(nn.Module):
             return self.norm(self.linear2(F.relu(self.linear1(x))) + self.proj(x))
 
 class Mixer(nn.Module):
-    def __init__(self, in_dim, hidden_dim, out_dim, temporal_length, conv=False):
+    def __init__(self, in_dim, hidden_dim, out_dim, temporal_length, conv=True):
         super().__init__()
+        self.conv = conv
         self.temporal_mlp = MLP(in_dim, hidden_dim, out_dim, conv=conv)
         # self.channel_mlp = MLP(temporal_length, int(temporal_length * 4), temporal_length)
         self.channel_mlp = MLP(temporal_length, hidden_dim, temporal_length)
