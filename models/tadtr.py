f24d10737149eba23b980e05ce33ac8bb2ef091c
Ho-Joong Kim <hojoong_kim@korea.ac.kr>
2023-01-31 13:41:22 +0900

    f

--- models/tadtr.py
+++ models/tadtr.py
@@ -192,6 +192,7 @@ class TadTR(nn.Module):
             # srcs.append(self.input_proj[i](src))
             srcs.append(src)
             masks.append(mask)
+            print(self.position_embedding(feat).size())
             pos.append(self.position_embedding(feat))
         # pos = [self.position_embedding(feat) for feat in features]
         # src, mask = features.tensors, features.mask
