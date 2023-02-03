7e49638f17eed1df11830b6bbc45d6829c65747b
Ho-Joong Kim <hojoong_kim@korea.ac.kr>
2023-02-04 05:22:46 +0900

    f

--- models/mypooler.py
+++ models/mypooler.py
@@ -215,6 +215,9 @@ class AdaptivePooler(nn.Module):
     def forward(self, x):
         pool_skip = self.pool_skip(x).flatten(2).transpose(-2, -1)
         x = rearrange(x, "b c ... -> b ... c")
+        print(pool_skip.size())
+        print(self.pool_proj(pool_skip).size())
+        print(self.drop_path(self.attn(self.norm1(x))).size())
         x = self.drop_path(self.attn(self.norm1(x))) + self.pool_proj(pool_skip)
         x = self.drop_path(self.mlp(self.norm2(x))) + self.proj(x)
         x = self.proj_norm(x)
