2e789f8ba63616d5715223b38a9c1aaaba3db1fd
Ho-Joong Kim <hojoong_kim@korea.ac.kr>
2023-02-03 10:19:06 +0900

    f

--- models/tadtr.py
+++ models/tadtr.py
@@ -204,18 +204,18 @@ class TadTR(nn.Module):
                 samples = nested_tensor_from_tensor_list(samples)  # (n, c, t)
 
         features = self.backbone(samples)
-        # srcs, masks, pos = [], [], []
-        # for i, feat in enumerate(features):
-        #     src, mask = feat.tensors, feat.mask
-        #     # srcs.append(self.input_proj[i](src))
-        #     srcs.append(src)
-        #     masks.append(mask)
-        #     pos.append(self.position_embedding(feat))
+        srcs, masks, pos = [], [], []
+        for i, feat in enumerate(features):
+            src, mask = feat.tensors, feat.mask
+            # srcs.append(self.input_proj[i](src))
+            srcs.append(src)
+            masks.append(mask)
+            pos.append(self.position_embedding(feat))
         # pos = [self.position_embedding(feat) for feat in features]
-        pos = [self.position_embedding(features)]
-        src, mask = features.tensors, features.mask
-        srcs = [proj(src) for proj in self.input_proj]
-        masks = [mask for _ in range(len(self.input_proj))]
+        # src, mask = features.tensors, features.mask
+        # srcs = [proj(src) for proj in self.input_proj]
+        # masks = [mask for _ in range(len(self.input_proj))]
+        # pos = [self.position_embedding(features)]
 
         query_embeds = None
         if self.two_stage:
