6f0884a40c0cf3dc705e2e79109d4e26cc3b37c1
Ho-Joong Kim <hojoong_kim@korea.ac.kr>
2023-02-08 01:35:43 +0900

    f

--- models/video_encoder_archs/slowfast.py
+++ models/video_encoder_archs/slowfast.py
@@ -466,8 +466,8 @@ class ResNet3dSlowFast(nn.Module):
             self.fast_poolers = nn.ModuleList([SpatialPooler('avg')])
             self.num_channels = 2304
         elif pooler == 'twpool':
-            self.slow_poolers = nn.ModuleList([SpatialPooler('twpool', input_dim=2048, base_dim=1024, num_layers=4)])
-            self.fast_poolers = nn.ModuleList([SpatialPooler('twpool', input_dim=256, base_dim=128, num_layers=4)])
+            self.slow_poolers = nn.ModuleList([SpatialPooler('twpool', input_dim=2048, base_dim=512, num_layers=4)])
+            self.fast_poolers = nn.ModuleList([SpatialPooler('twpool', input_dim=256, base_dim=64, num_layers=4)])
             self.num_channels = 576
             # self.num_channels = 1152
         else:
