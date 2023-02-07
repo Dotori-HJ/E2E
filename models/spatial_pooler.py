6072e9c8c15a6dfa6450f0a9edee93d99c05c75c
Ho-Joong Kim <hojoong_kim@korea.ac.kr>
2023-02-08 01:32:11 +0900

    f

--- models/spatial_pooler.py
+++ models/spatial_pooler.py
@@ -184,10 +184,10 @@ class TemporalWiseAttentionPooling(nn.Module):
         output_dim=None,
         num_heads=8,
         mlp_ratio=4.0,
-        qkv_bias=True,
+        qkv_bias=False,
         drop_rate=0.0,
         drop_path=0.0,
-        act_layer=nn.ReLU,
+        act_layer=nn.GELU,
         norm_layer=nn.LayerNorm,
         num_layers=4,
         skip='max'
