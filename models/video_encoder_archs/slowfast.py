3bda0c1c9c42ce5ada14964a36ed10c9be7bcc4b
Ho-Joong Kim <hojoong_kim@korea.ac.kr>
2023-02-07 13:37:14 +0900

    f

--- models/video_encoder_archs/slowfast.py
+++ models/video_encoder_archs/slowfast.py
@@ -467,8 +467,8 @@ class ResNet3dSlowFast(nn.Module):
             self.fast_poolers = nn.ModuleList([SpatialPooler('avg')])
             self.num_channels = 2304
         elif pooler == 'twpool':
-            self.slow_poolers = nn.ModuleList([SpatialPooler('twpool', input_dim=2048, base_dim=1024, num_layers=6)])
-            self.fast_poolers = nn.ModuleList([SpatialPooler('twpool', input_dim=256, base_dim=128, num_layers=6)])
+            self.slow_poolers = nn.ModuleList([SpatialPooler('twpool', input_dim=2048, base_dim=1024, num_layers=8)])
+            self.fast_poolers = nn.ModuleList([SpatialPooler('twpool', input_dim=256, base_dim=128, num_layers=8)])
             self.num_channels = 1152
         else:
             assert NotImplementedError
