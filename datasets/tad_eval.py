87c60b422fb2b269b8212f419bdbc47f60875710
Ho-Joong Kim <hojoong_kim@korea.ac.kr>
2023-02-19 18:20:18 +0900

    f

--- datasets/tad_eval.py
+++ datasets/tad_eval.py
@@ -172,7 +172,6 @@ class TADEvaluator(object):
 
                 # On ActivityNet, follow the tradition to use external video label
                 if assign_cls_labels:
-                # if self.dataset_name == 'activitynet':
                     topk = 2
 
                     cls_scores = np.asarray(self.cls_scores[video_id])
@@ -187,6 +186,12 @@ class TADEvaluator(object):
                     new_pred_segment = np.tile(dets[:, :2], (topk, 1))
                     new_pred_label = np.tile(topk_cls_idx[:, None], (1, len(dets))).flatten()[:, None]
                     dets = np.concatenate((new_pred_segment, new_pred_score, new_pred_label), axis=-1)
+                elif self.dataset_name == 'activitynet':
+                    topk = 2
+
+                    cls_scores = np.asarray(self.cls_scores[video_id])
+                    topk_cls_idx = np.argsort(cls_scores)[::-1][:topk]
+                    dets = torch.cat([dets[dets[:, 3] != idx] for idx in topk_cls_idx])
 
                 min_score = 0.001
                 dets = dets[dets[:, 2] > min_score]
