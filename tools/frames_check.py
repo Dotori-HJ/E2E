48fadd36a97fb8db43b2f83b57a8e6bff16c7cde
Ho-Joong Kim <hojoong_kim@korea.ac.kr>
2023-02-21 13:52:55 +0900

    f

---
+++ tools/frames_check.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+if __name__ == '__main__':
+    anno_path = 'data/activitynet/activity_net.v1-3.min.json'
+    with open(anno_path) as f:
+        anno_dict = json.load(f)['database']
+
+    # src_folder = 'data/activitynet/384frames'
+    src_folder = 'data/frames/activitynet'
+    for vid in anno_dict.keys():
+        src_vid_folder = os.path.join(src_folder, 'v_' + vid)
+        frame_names = os.listdir(src_vid_folder)
+        num_frames = len(frame_names)
+        if num_frames == 0:
+            print(vid, num_frames)
