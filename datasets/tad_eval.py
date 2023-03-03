26bef79adf70d78f9663d24c67393e32ae9fb743
Ho-Joong Kim <hojoong_kim@korea.ac.kr>
2023-03-03 11:24:46 +0900

    f

--- datasets/tad_eval.py
+++ datasets/tad_eval.py
@@ -228,7 +228,6 @@ class TADEvaluator(object):
                     new_pred_label = np.tile(topk_cls_idx[:, None], (1, len(dets))).flatten()[:, None]
                     new_dets = np.concatenate((new_pred_segment, new_pred_score, new_pred_label), axis=-1)
 
-
                     voting_thresh = 0.75
                     if voting_thresh > 0:
                         new_segs = seg_voting(
@@ -237,7 +236,7 @@ class TADEvaluator(object):
                             dets[:, 2],
                             voting_thresh
                         )
-                        dets = np.concatnate((new_segs, new_dets[:, 2:]), axis=1)
+                        dets = np.concatenate((new_segs, new_dets[:, 2:]), axis=1)
                     else:
                         dets = new_dets
 
